cf2e88bf8ea85b11926d222d4471d7169ff5044f
vicky2293 <vignesh.ramachandran2207@gmail.com>
2026-05-31 15:01:16 +0200

    Enable env variable support

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,11 +1,11 @@
 export class Constants {
-  static readonly BASE_URL = "/";
-  static readonly INVENTORY_URL = "/inventory.html";
-  static readonly CART_URL = "/cart.html";
-  static readonly CHECKOUT_STEP_ONE_URL = "/checkout-step-one.html";
-  static readonly CHECKOUT_STEP_TWO_URL = "/checkout-step-two.html";
-  static readonly CHECKOUT_COMPLETE_URL = "/checkout-complete.html";
-  
-  static readonly VALID_USERNAME = "standard_user";
-  static readonly VALID_PASSWORD = "secret_sauce";
+  static readonly BASE_URL = process.env.BASE_URL!;
+  static readonly INVENTORY_URL = process.env.INVENTORY_URL!;
+  static readonly CART_URL = process.env.CART_URL!;
+  static readonly CHECKOUT_STEP_ONE_URL = process.env.CHECKOUT_STEP_ONE_URL!;
+  static readonly CHECKOUT_STEP_TWO_URL = process.env.CHECKOUT_STEP_TWO_URL!;
+  static readonly CHECKOUT_COMPLETE_URL = process.env.CHECKOUT_COMPLETE_URL!;
+
+  static readonly VALID_USERNAME = process.env.VALID_USERNAME!;
+  static readonly VALID_PASSWORD = process.env.VALID_PASSWORD!;
 }
