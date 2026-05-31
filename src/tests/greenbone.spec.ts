37f73e3a9d79653f04d7cd182840adad9c15d90c
vicky2293 <vignesh.ramachandran2207@gmail.com>
2026-05-31 14:37:56 +0200

    Add first test

---
+++ src/tests/greenbone.spec.ts
@@ -0,0 +1,14 @@
+import { test } from '../fixtures/fixture';
+import { Constants } from '../utils/constants';
+
+test.describe('greenbone assignment', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(Constants.BASE_URL);
+  });
+
+  test('Verify whether the `standard_user` is able to log in with valid credentials.', async ({loginPage, inventoryPage}) => {
+    await loginPage.visitLoginPage();
+    await loginPage.login(Constants.VALID_USERNAME, Constants.VALID_PASSWORD);
+    await inventoryPage.verifyInventoryPage();
+  });
+});
