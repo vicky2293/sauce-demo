c3726d84d65544205dec03e31d15947d7f8b5f33
vicky2293 <vignesh.ramachandran2207@gmail.com>
2026-05-31 14:58:19 +0200

    Add e2e test

--- src/fixtures/fixture.ts
+++ src/fixtures/fixture.ts
@@ -1,11 +1,20 @@
 import { test as base } from '@playwright/test';
 import { LoginPage } from '../pageObjects/loginPage';
 import { InventoryPage } from '../pageObjects/inventoryPage';
-
+import { CartPage } from '../pageObjects/cartPage';
+import { CheckoutStepOnePage } from '../pageObjects/checkoutStepOnePage';
+import { CheckoutStepTwoPage } from '../pageObjects/checkoutStepTwoPage';
+import { CheckoutCompletePage } from '../pageObjects/checkoutComplete';
+import { CommonPage } from '../pageObjects/commonPage';
 
 type MyFixtures = {
   loginPage: LoginPage;
   inventoryPage: InventoryPage;
+  cartPage: CartPage;
+  checkoutStepOnePage: CheckoutStepOnePage;
+  checkoutStepTwoPage: CheckoutStepTwoPage;
+  checkoutCompletePage: CheckoutCompletePage;
+  commonPage: CommonPage;
 };
 
 export const test = base.extend<MyFixtures>({
@@ -14,7 +23,22 @@ export const test = base.extend<MyFixtures>({
   },
   inventoryPage: async ({ page }, use) => {
     await use(new InventoryPage(page));
-  }
+  },
+  cartPage: async ({ page }, use) => {
+    await use(new CartPage(page));
+  },
+  checkoutStepOnePage: async ({ page }, use) => {
+    await use(new CheckoutStepOnePage(page));
+  },
+  checkoutStepTwoPage: async ({ page }, use) => {
+    await use(new CheckoutStepTwoPage(page));
+  },
+  checkoutCompletePage: async ({ page }, use) => {
+    await use(new CheckoutCompletePage(page));
+  },
+  commonPage: async ({ page }, use) => {
+    await use(new CommonPage(page));
+  },
 });
 
 export { expect } from '@playwright/test';
--- src/tests/greenbone.spec.ts
+++ src/tests/greenbone.spec.ts
@@ -1,10 +1,52 @@
 import { test } from '../fixtures/fixture';
 import { Constants } from '../utils/constants';
+import { CheckoutData } from '../utils/checkoutData';
 
 test.describe('greenbone assignment', () => {
   test('Verify whether the `standard_user` is able to log in with valid credentials.', async ({loginPage, inventoryPage}) => {
     await loginPage.visitLoginPage();
     await loginPage.login(Constants.VALID_USERNAME, Constants.VALID_PASSWORD);
     await inventoryPage.verifyInventoryPage();
+  });
+
+  test('Verify whether the `standard_user` is able to place an order successfully with valid details.', async ({loginPage, inventoryPage, cartPage, checkoutStepOnePage, checkoutStepTwoPage, checkoutCompletePage, commonPage}) => {
+    await test.step('visit saucedemo webpage', async () => {
+      await loginPage.visitLoginPage();
+      await loginPage.login(Constants.VALID_USERNAME, Constants.VALID_PASSWORD);
+      await inventoryPage.verifyInventoryPage();
+    });
+
+    await test.step('add product to cart', async () => {
+      await inventoryPage.addToCartButton('Sauce Labs Backpack');
+      await inventoryPage.addToCartButton('Sauce Labs Bike Light');
+      await inventoryPage.clickShoppingCart();
+      await cartPage.verifyCartPage();
+    });
+
+    await test.step('verify product in cart', async () => {
+      await cartPage.verifyProductInCart('Sauce Labs Backpack');
+      await cartPage.verifyProductInCart('Sauce Labs Bike Light');
+      await cartPage.clickCheckoutButton();
+      await checkoutStepOnePage.verifyCheckoutStepOnePage();
+    });
+
+    await test.step('fill checkout information', async () => {
+      await checkoutStepOnePage.fillCheckoutForm(CheckoutData.FIRST_NAME, CheckoutData.LAST_NAME, CheckoutData.ZIP_CODE);
+      await checkoutStepOnePage.clickContinueButton();
+      await checkoutStepTwoPage.verifyCheckoutStepTwoPage();
+    });
+
+    await test.step('finish checkout', async () => {
+      await checkoutStepTwoPage.verifyProductInCart('Sauce Labs Backpack');
+      await checkoutStepTwoPage.verifyProductInCart('Sauce Labs Bike Light');
+      await checkoutStepTwoPage.clickFinishButton();
+      await checkoutCompletePage.verifySuccessMessage();
+    });
+
+    await test.step('verify order confirmation', async () => {
+    await checkoutCompletePage.verifyCheckoutPage();
+    await checkoutCompletePage.verifySuccessMessage();
+    await commonPage.logout();
+    });
   });
 });
