713469de6c975c8ae971722e1f6557f0588ee718
vicky2293 <vignesh.ramachandran2207@gmail.com>
2026-05-31 14:37:07 +0200

    Add util constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,7 @@
+export class Constants {
+  static readonly BASE_URL = "https://www.saucedemo.com/";
+  static readonly INVENTORY_URL = "https://www.saucedemo.com/inventory.html";
+  
+  static readonly VALID_USERNAME = "standard_user";
+  static readonly VALID_PASSWORD = "secret_sauce";
+}
